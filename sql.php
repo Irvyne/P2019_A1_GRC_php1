2f575917684490602270e7b80f3e145aae196355
Thibaud BARDIN <thibaud.bardin@gmail.com>
2015-02-03 13:02:56 +0100

    Update

--- sql.php
+++ sql.php
@@ -1,2 +1,18 @@
 <?php
 
+$config = [
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => 'toor', // WAMP => '' | MAMP => 'root'
+    'port'     => null, // WAMP => null | MAMP => 8889
+    'dbname'   => 'p2019_grc_php1',
+];
+
+$link = mysqli_connect(
+    $config['hostname'],
+    $config['username'],
+    $config['password'],
+    $config['dbname'],
+    $config['port']
+) or die('Erreur');
+
