6907419ace7f14903e9d63e5edc896a43df00dcb
Thibaud BARDIN <thibaud.bardin@gmail.com>
2015-02-05 11:29:05 +0100

    Update

--- sql.php
+++ sql.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'function/database.fn.php';
+
 $config = [
     'hostname' => 'localhost',
     'username' => 'root',
@@ -8,11 +10,25 @@ $config = [
     'dbname'   => 'p2019_grc_php1',
 ];
 
-$link = mysqli_connect(
-    $config['hostname'],
-    $config['username'],
-    $config['password'],
-    $config['dbname'],
-    $config['port']
-) or die('Erreur');
+$link = database_connect($config);
 
+// SELECT | INSERT | UPDATE | DELETE
+$sql = 'SELECT * FROM pokemon';
+
+$query = mysqli_query($link, $sql);
+
+$pokemons = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
+foreach ($pokemons as $pokemon) {
+    //var_dump($pokemon);
+    echo '<h1>'.$pokemon['name'].'</h1>';
+    // TODO Afficher nom du pok dans un <h1>
+    echo '<h2>'.$pokemon['hp'].'HP</h2>';
+    // TODO Afficher hp du pok dans un <h2>
+}
+
+/*
+while (null !== $result = mysqli_fetch_assoc($query)) {
+    var_dump($result);
+}
+*/
